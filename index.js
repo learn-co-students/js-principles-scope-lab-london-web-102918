a6e8a51e6098fed580d4210863932272d2098a75
rosen777 <rosen.toshev@flatironschool.com>
2018-12-09 18:48:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = '1';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  return bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  return leastFavoriteCustomer = '2';
+}
